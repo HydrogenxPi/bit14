34ff274a951f1786c65b9371582ef51f9d686db7
HydrogenxPi <agrem44@gmail.com>
2024-10-24 20:51:32 -0400

    Add files via upload

--- bit14/bit14_preprocessor.h
+++ bit14/bit14_preprocessor.h
@@ -48,11 +48,11 @@ defined(_M_IX86) || defined(__LP64__) ||defined(_M_AMD64) || defined(_M_X64) ||\
 #define BIT14_USING_X86_OR_X64
 #endif
 
+#if defined(__arm__) || defined(__aarch64__) || defined(_M_ARM64) || defined(_M_ARM)
+#define BIT14_USING_ARM
+#endif
+
 #if defined(_WIN64) || defined(__x86_64) || defined(__x86_64__) || defined(__LP64__ ) ||\
  defined(__LP64) || defined(_M_X64) || defined(_M_ARM64)
 #define BIT14_USING_64BIT
-#endif
-
-#if defined(__arm__) || defined(__aarch64__) || defined(_M_ARM64) || defined(_M_ARM)
-#define BIT14_USING_ARM
 #endif
